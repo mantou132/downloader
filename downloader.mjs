65e9141924d1141e20d682275e3ca8c29e413343
王先硚 <709922234@qq.com>
2019-02-24 02:34:59 +0800

    feat: add error handle

--- downloader.mjs
+++ downloader.mjs
@@ -1,14 +1,23 @@
 import puppeteer from 'puppeteer';
 import shell from 'shelljs';
+import ora from 'ora';
 import { URL } from 'url';
 
 const FETCH_COMPLETE = 'fetch complete';
+const FETCH_FAIL = 'fetch fail';
 
 export default async function downloader(pkg, output) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  });
+  const spinner = ora(`fetching ${pkg}`).start();
+
+  let browser;
+  try {
+    browser = await puppeteer.launch({});
+  } catch {
+    browser = await puppeteer.launch({
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    });
+  }
+
   const page = await browser.newPage();
 
   let dir;
@@ -25,20 +34,29 @@ export default async function downloader(pkg, output) {
 
   await page.goto('about:blank', { waitUntil: 'load' });
   await page.evaluate(
-    (pkgName, msg) => {
-      import(`https://dev.jspm.io/${pkgName}`).then(() => {
-        console.log(msg);
-      });
+    (pkgName, complete, fail) => {
+      import(`https://dev.jspm.io/${pkgName}`)
+        .then(() => console.log(complete))
+        .catch(() => console.log(fail));
     },
     pkg,
     FETCH_COMPLETE,
+    FETCH_FAIL,
   );
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     page.on('console', async (msg) => {
       if (msg.text() === FETCH_COMPLETE) {
         await browser.close();
         resolve({ dir });
+        spinner.stop();
+        spinner.succeed(`installed ${pkg}`);
+      }
+      if (msg.text() === FETCH_FAIL) {
+        await browser.close();
+        reject();
+        spinner.stop();
+        spinner.fail(`install failed ${pkg}`);
       }
     });
   });
